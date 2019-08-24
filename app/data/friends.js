6984582d0e571c3a5eba29404e3269c6663bf3a5
Jesse McKinney <jessman51386@gmail.com>
2019-08-24 16:25:40 -0400

    added listener, frinds list, files and folders, htmls and questions

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friends = [
+    {
+        name: "Inky",
+        photo: "",
+        scores: [3, 2, 3, 5, 2, 4, 4, 3, 2, 4]
+    },
+    {
+        name: "Winky",
+        photo: "",
+        scores: [2, 4, 5, 3, 5, 5, 4, 3, 2, 2]
+    },
+    {
+        name: "Blinky",
+        photo: "",
+        scores: [5, 3, 1, 4, 3, 1, 4, 5, 5, 1]
+    }, 
+    {
+        name: "Clyde",
+        photo: "",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    }
+];
+module.exports = friends;
