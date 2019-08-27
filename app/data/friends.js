3b857ed3aadf0b115dc743d053b9d05513154f97
Jesse McKinney <jessman51386@gmail.com>
2019-08-27 18:02:57 -0400

    fixed the default pacman ghost images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friends = [
     {
         name: "Inky",
-        photo: "images/inky.png",
+        photo: "https://static.giantbomb.com/uploads/square_small/8/87790/2469741-inky.png",
         scores: ["3", "2", "3", "5", "2", "4", "4", "3", "2", "4"]
     },
     {
         name: "Pinky",
-        photo: "images/pinky.png",
+        photo: "https://giantbomb1.cbsistatic.com/uploads/square_small/8/87790/2469744-pinky.png",
         scores: ["2", "4", "5", "3", "5", "5", "4", "3", "2", "2"]
     },
     {
         name: "Blinky",
-        photo: "images/blinky.png",
+        photo: "https://static.giantbomb.com/uploads/square_small/8/87790/2469740-blinky.png",
         scores: ["5", "3", "1", "4", "3", "1", "4", "5", "5", "1"]
     }, 
     {
         name: "Clyde",
-        photo: "images/clyde.png",
+        photo: "https://static.giantbomb.com/uploads/square_small/8/87790/2469743-orange.png",
         scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
     }
 ];
